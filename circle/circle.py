a39121a987a14d69aabbe470ef4ac9a22c3d0ecb
Jan Fincke <janfincke1@gmail.com>
2020-07-22 00:30:01 +0300

    Finished circle task (all bonus tasks included)

--- circle/circle.py
+++ circle/circle.py
@@ -3,18 +3,43 @@ from math import pi
 
 class Circle(object):
     def __init__(self, radius=1):
-        self.radius = radius
-        self.diameter = self._get_diameter()
-        self.area = self._get_area()
+        self._radius = radius
+        self._diameter = 0
+        self._area = 0
 
     def __str__(self):
-        return 'Circle({})'.format(str(self.radius))
+        return 'Circle({})'.format(str(self._radius))
 
     def __repr__(self):
-        return 'Circle({})'.format(str(self.radius))
+        return 'Circle({})'.format(str(self._radius))
 
-    def _get_diameter(self):
-        return self.radius*2
+    @property
+    def radius(self):
+        return self._radius
 
-    def _get_area(self):
-        return (self.radius ** 2) * pi
+    @radius.setter
+    def radius(self, value):
+        if value < 0:
+            raise ValueError("Radius cannot be negative")
+        self._radius = value
+
+    @property
+    def diameter(self):
+        self._diameter = self._radius * 2
+        return self._diameter
+
+    @diameter.setter
+    def diameter(self, value):
+        if value < 0:
+            raise ValueError("Diameter cannot be negative")
+        self._diameter = value * 2
+        self._radius = value / 2
+
+    @property
+    def area(self):
+        self._area = (self._radius ** 2) * pi
+        return self._area
+
+    @area.setter
+    def area(self, value):
+        raise AttributeError("can't set attribute")
--- circle/main.py
+++
@@ -1,7 +0,0 @@
-from circle import Circle
-
-c = Circle(5)
-print(c.radius)
-print(c.diameter)
-print(c.area)
-
