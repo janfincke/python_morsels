a8c2acd0ba95d5efdca4fc510e8ebe2bec4857bd
Jan Fincke <janfincke1@gmail.com>
2020-07-22 00:29:21 +0300

    Modified add

--- add/add.py
+++ add/add.py
@@ -1,2 +1,8 @@
 def add(*args):
     return list(list(sum(j) for j in zip(*rows)) for rows in zip(*args))
+
+
+matrix1 = [[1, -2], [-3, 4]]
+matrix2 = [[2, -1], [0, -1]]
+result = add(matrix1, matrix2)
+print(result)
